482a5c754196c74a7ca7b91bc4c399c7522bb822
kshumish <kshumish@ucsc.edu>
2019-10-21 15:14:16 -0700

    Updating Django library

--- nbaStat/manage.py
+++ nbaStat/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'local_lib.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nbaStat.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
