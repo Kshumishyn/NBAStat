67cd0c1d82e0a1929037330fa472f35fa05ce8b1
kshumish <kshumish@ucsc.edu>
2019-10-30 18:12:51 -0700

    MEERRGEE

--- nbaStat/nbaStat/views.py
+++ nbaStat/nbaStat/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
-
-def index(request):
+def homePage(request):
     return render(request, 'index.html')
+
+def playerPage(request):
+    return render(request, 'player.html')
